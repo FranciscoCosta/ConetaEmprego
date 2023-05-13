ba1bb4b6bf976484c796bd149339d4351890f45d
Xico <francisco100eg@gmail.com>
2023-05-13 13:30:53 -0300

    Doc: Learning Apis Laravel

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/post', function(){
+//     return response()-> json([
+//         'post' => [
+//             'title' => 'Post Title',
+//             'body' => 'Post Body'
+//         ]
+
+//     ]);
+// });
